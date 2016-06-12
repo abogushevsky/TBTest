1ca63eac2824e58f84dbabb75d3cd6ee814608e6
Anton Bogushevsky <anton.bogushevsky@gmail.com>
2016-06-12 11:08:51 +0300

    Добавлен класс задачи и скрипты создания таблиц

---
+++ src/TaskManager.Common/Entities/Task.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TaskManager.Common.Entities
+{
+    /// <summary>
+    /// Задача
+    /// </summary>
+    public class Task : EntityBase
+    {
+        /// <summary>
+        /// Название задачи или краткое описание
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Детальное описание задачи
+        /// </summary>
+        public string Details { get; set; }
+
+        /// <summary>
+        /// Крайний срок выполнения задачи
+        /// </summary>
+        public DateTime DueDate { get; set; }
+
+        /// <summary>
+        /// Категория задачи
+        /// </summary>
+        public Category Category { get; set; }
+
+        /// <summary>
+        /// Идентификатор пользователя, которому принадлежит задача
+        /// </summary>
+        public string UserId { get; set; }
+    }
+}
